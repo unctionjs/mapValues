4b62b1194709c2e51cf9df3310f3148adb740e15
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-11-24 22:45:12 -0800

    Fixing a massive bug in how we handle streams and strings

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import mapValuesWithValueKey from "@unction/mapvalueswithvaluekey"
 
-export default function mapValues (unction: ValueType => ValueType): Function {
-  return mapValuesWithValueKey((value: ValueType): Function => (): ValueType => unction(value))
+export default function mapValues (unction: MapFunctionType): UnaryFunctionType {
+  return function mapValuesUnction (functor: FunctorType): FunctorType {
+    if (functor.map instanceof Function) {
+      return functor.map((value: ValueType): ValueType => unction(value))
+    }
+
+    return mapValuesWithValueKey((value: ValueType): UnaryFunctionType => (): ValueType => unction(value))(functor)
+  }
 }
--- test.js
+++ test.js
@@ -1,23 +1,75 @@
-/* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-magic-numbers */
+/* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, flowtype/require-variable-type, no-magic-numbers */
 import {test} from "tap"
+
+import xstream from "xstream"
+import streamSatisfies from "@unction/streamsatisfies"
 
 import mapValues from "./index"
 
-test(({same, end}) => {
+test("String", ({equal, end}) => {
+  equal(
+    mapValues(
+      (value) => `a${value}`
+    )(
+      "abc"
+    ),
+    "aaabac"
+  )
+
+  end()
+})
+
+test("Set", ({same, end}) => {
   same(
-    mapValues((value) => value + 1)([1, 2, 3]),
+    mapValues(
+      (value) => value + 1
+    )(
+      new Set([1, 2, 3])
+    ),
+    new Set([2, 3, 4])
+  )
+
+  end()
+})
+
+test("Map", ({same, end}) => {
+  same(
+    mapValues(
+      (value) => value + 1
+    )(
+      new Map([[1, 1], [2, 2], [3, 3]])
+    ),
+    new Map([[1, 2], [2, 3], [3, 4]])
+  )
+
+  end()
+})
+
+
+test("Array", ({same, end}) => {
+  same(
+    mapValues(
+      (value) => value + 1
+    )(
+      [1, 2, 3]
+    ),
     [2, 3, 4]
   )
 
   end()
 })
 
-test(({same, end}) => {
+test("Object", ({same, end}) => {
   same(
-    mapValues((value) => value + 1)({
-      age: 29,
-      interval: 10,
-    }),
+    mapValues(
+      (value) => value + 1
+    )(
+      {
+        age: 29,
+        interval: 10,
+      }
+
+    ),
     {
       age: 30,
       interval: 11,
@@ -25,4 +77,36 @@ test(({same, end}) => {
   )
 
   end()
+})
+
+test("Stream", ({same, end}) => {
+  streamSatisfies(
+    "30---|"
+  )(
+    (given) => (expected) => same(given, expected)
+  )(
+    () => () => end()
+  )(
+    mapValues(
+      (value) => value + 1
+    )(
+      xstream.of(29)
+    )
+  )
+})
+
+test("MemoryStream", ({same, end}) => {
+  streamSatisfies(
+    "30---|"
+  )(
+    (given) => (expected) => same(given, expected)
+  )(
+    () => () => end()
+  )(
+    mapValues(
+      (value) => value + 1
+    )(
+      xstream.of(29).remember()
+    )
+  )
 })
